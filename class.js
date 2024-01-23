085c307634880ccbbd62eefa4bf8d22e0fe0c6c4
2sammy <samuelmanoah61@gmail.com>
2024-01-23 21:17:36 +0300

    class

---
+++ class.js
@@ -0,0 +1,14 @@
+class User {
+    constructor(name, email) {
+        this.name = name,
+        this.email = email
+
+    }
+
+
+
+
+}
+
+var userOne = new User("sam", "sammy@gmail.com");
+var userTwo = new User("mike", "mike@gmail.com")
