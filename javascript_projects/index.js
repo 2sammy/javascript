3b19c33b015ee6fb1ad93062555f5dabe07b99ba
2sammy <samuelmanoah61@gmail.com>
2024-02-27 18:04:51 +0300

    variables

---
+++ javascript_projects/index.js
@@ -0,0 +1,29 @@
+//console.log('hello')
+//window.alert('hello sam')
+
+//document.getElementById("myH1").textContent = 'shalom';
+//document.getElementById("myP").textContent = "damn";
+//document.getElementById('myH1').textContent = "shalom";
+
+//variables
+/*let name = "sam"
+let gpa = 6.88
+let price = 10.99
+let age = 20
+console.log(`the price of pizza is $${price}`);
+console.log(` you score of the term was ${gpa}`);
+console.log(`i will be turning ${age} years old`);
+console.log(typeof name);
+console.log(typeof age);
+*/
+ 
+//consts
+const PI = 3.142
+let radius;
+let circumference;
+
+radius = window.prompt('Enter radius');
+radius = Number(radius);
+
+circumference = 2*PI*radius;
+console.log('the cirm is:', circumference);
