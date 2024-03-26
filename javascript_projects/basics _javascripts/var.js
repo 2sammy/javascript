92477c092ef2e33d2d0b5b3e57ba8dbfb84684e6
2sammy <samuelmanoah61@gmail.com>
2024-03-26 21:44:17 +0300

    updates

---
+++ javascript_projects/basics _javascripts/var.js
@@ -0,0 +1,25 @@
+//let firstName = "sammy";
+
+//const names = Array()
+//or
+// syntax
+// This the most recommended way to create an empty list
+//const arr = []
+//console.log(arr)
+
+const colors = ["green", "yellow"]
+console.log(colors.length)
+
+//concat:To concatenate two arrays.
+
+const firstList = [1, 2, 3]
+const secondList = [4, 5, 6]
+const thirdList = firstList.concat(secondList)
+
+console.log(thirdList) // [1, 2, 3, 4, 5, 6]
+
+const firstName =["sammy"]
+const lastName =["manoah"]
+
+const fullName = firstName.concat(lastName);
+console.log(fullName)
