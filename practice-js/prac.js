2d3740834f6c7bd1786c026283ecf59c64b09224
2sammy <samuelmanoah61@gmail.com>
2024-03-26 21:44:52 +0300

    pract

---
+++ practice-js/prac.js
@@ -0,0 +1,23 @@
+//comments can make code readable
+//welcome to 30DaysOfjavascript
+
+/**comments can make code readable, 
+ * easy to reuse and informative */
+//non-primitive data types
+let nums =[1,2,3];
+nums[0] = 10
+
+let nums = [1, 2, 3]
+let numbers = nums
+
+console.log(nums == numbers)  // true
+
+let userOne = {
+name:'manoah',
+role:'millionaire',
+country:'usa'
+}
+
+let userTwo = userOne
+
+console.log(userOne == userTwo)  // true
